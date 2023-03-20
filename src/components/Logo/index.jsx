3fd726e32d3924deb9b5b4d6437b5e5c931f39a4
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-03-20 21:01:09 +0200

    deleting unnecessary import @Logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,18 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
+import LogoImg from "../../assets/images/Logo.png";
 
-import LogoImg from '../../assets/images/Logo.png'; 
-
-
-
-
-function index({width , height}) {
+function index({ width, height }) {
   return (
     <>
-       <img width={width} height={height} src={LogoImg} alt="logo"/>
+      <img width={width} height={height} src={LogoImg} alt='logo' />
     </>
-  )
+  );
 }
 
-export default index
+export default index;
