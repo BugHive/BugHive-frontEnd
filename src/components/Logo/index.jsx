99c06e190ce91d6e566a7b33d5250c2441b201ff
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-03-20 20:57:59 +0200

    Fix logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -4,15 +4,14 @@ import styled from 'styled-components';
 
 import LogoImg from '../../assets/images/Logo.png'; 
 
-const Logo = styled.div`
-  
 
-`;
+
+
 function index({width , height}) {
   return (
-    <Logo>
+    <>
        <img width={width} height={height} src={LogoImg} alt="logo"/>
-    </Logo>
+    </>
   )
 }
 
