af9d0ceb1fb3f6867f16121c71d85f87f41637aa
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-03-20 19:33:00 +0200

    creating theme objects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./styles/theme";
 
 
 function App() {
   return (
+    <ThemeProvider theme={darkTheme}>
     <div className="App">
       Hello World!
     </div>
+    </ThemeProvider>
   );
 }
 
