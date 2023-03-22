7346c08d3384b638c6b01d79924c2e68e1c1c9a2
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-03-22 18:25:25 +0200

    Fixing logo & adding different button colors

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { StyledButton } from "./style";
 
 export default function Button(props) {
-  return <StyledButton>{props.title}</StyledButton>;
+  return <StyledButton switched={props.switched}>{props.title}</StyledButton>;
 }
