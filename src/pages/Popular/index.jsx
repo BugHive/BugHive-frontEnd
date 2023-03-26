51a61349c01564164abd1f40bb530c39635aa63e
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-03-27 02:11:49 +0300

    Add popular nested page

---
+++ src/pages/Popular/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div style={{paddingBottom:'10em'}}>
+        <h1>
+           Popular
+        </h1>
+    </div>
+  )
+}
+
+export default index
