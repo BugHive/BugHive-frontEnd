fe3af9c14fff8c1d1c27954eabdae6140b755819
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-05-14 22:59:57 +0300

    creating page and components

---
+++ src/pages/Tags/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Tags() {
+  const Container = styled.div`
+    padding: 2rem;
+  `;
+  return (
+    <Container>
+      <h3>Tags Board</h3>
+    </Container>
+  );
+}
