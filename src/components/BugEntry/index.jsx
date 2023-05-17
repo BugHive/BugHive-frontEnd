8a01bc0c9efacc06f798740b7bcaa3c6acadcf4d
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-05-17 20:45:11 +0300

    finishing tags page

--- src/components/BugEntry/index.jsx
+++ src/components/BugEntry/index.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function BugEntry() {
-  return <div>BugEntry</div>;
+export default function BugEntry(props) {
+  const Entry = styled.div`
+    background-color: ${(props) => props.theme.palette.primary};
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+
+    .transparent {
+      opacity: 0.5;
+      margin-bottom: 2.5px;
+    }
+
+    .space {
+      margin-bottom: 1rem;
+    }
+  `;
+  return (
+    <Entry>
+      <p className='transparent'>NAME</p>
+      <p className='space'>{props.name}</p>
+      <p className='transparent'>LAST MODIFIED</p>
+      <p>{props.date}</p>
+    </Entry>
+  );
 }
--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import Add from "../../assets/images/add.png";
 
-export default function Tag() {
-  return <div>Tag</div>;
+export default function Tag(props) {
+  const TagContainer = styled.div`
+    background-color: ${(props) => props.theme.palette.secondary};
+    display: grid;
+    grid-auto-rows: min-content;
+    gap: 1rem;
+    padding: 1rem;
+    margin-top: 2rem;
+
+    .name {
+      font-weight: bolder;
+      font-size: 20px;
+    }
+    .add-entry {
+      margin-top: 2rem;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.5rem;
+      opacity: 0.5;
+      img {
+        height: 1rem;
+      }
+    }
+  `;
+  return (
+    <TagContainer>
+      <p className='name'>{props.name}</p>
+      {props.entries}
+      <div className='add-entry'>
+        <img src={Add} alt='add' />
+        <p>Add New Bug Entry</p>
+      </div>
+    </TagContainer>
+  );
 }
