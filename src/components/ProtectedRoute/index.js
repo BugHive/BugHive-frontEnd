8c4ec44eb3882a9547d17d5d3751ef2637779c94
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-05-19 21:09:43 +0300

    fixing minor issue at protected route

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
 export default function ProtectedRoute() {
-  const isLoggedIn = useContext(AuthContext);
+  const { isLoggedIn } = useContext(AuthContext);
 
   if (!isLoggedIn) {
     return <Navigate to={"/login"} />;
