57207b02b4afe2e4e95dacdbaa87850451e49a7f
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-05-21 19:59:44 +0300

    minor changes

--- src/services/users.js
+++ src/services/users.js
@@ -1,47 +1,46 @@
-import axios from 'axios'
-const baseUrl = '/api/users'
-let token = null
-
+import axios from "axios";
+const baseUrl = "/api/users";
+let token = null;
 
 const setToken = (newToken) => {
-  token = `bearer ${newToken}`
-}
+  token = `bearer ${newToken}`;
+};
 
 const getAll = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-}
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
 
 const get = async (id) => {
   const config = {
     headers: { Authorization: token },
-  }
+  };
 
-  const response = await axios.get(`${baseUrl}/${id}`, config)
-  return response.data
-}
+  const response = await axios.get(`${baseUrl}/${id}`, config);
+  return response.data;
+};
 
 const create = async (newUser) => {
-  const response = await axios.post(baseUrl, newUser)
-  return response.data
-}
+  const response = await axios.post(baseUrl, newUser);
+  return response.data;
+};
 
 const remove = async (id) => {
   const config = {
-    headers: { Authorization: token }
-  }
+    headers: { Authorization: token },
+  };
 
-  const response = await axios.delete(`${baseUrl}/${id}`, config)
-  return response.data
-}
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+  return response.data;
+};
 
 const update = async (id, newUser) => {
   const config = {
-    headers: { Authorization: token }
-  }
+    headers: { Authorization: token },
+  };
 
-  const response = await axios.put(`${baseUrl}/${id}}`, newUser, config)
-  return response.data
-}
+  const response = await axios.put(`${baseUrl}/${id}}`, newUser, config);
+  return response.data;
+};
 
-export default { setToken, getAll, get, create, remove, update }
+export { setToken, getAll, get, create, remove, update };
