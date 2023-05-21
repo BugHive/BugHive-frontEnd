8ff25908b0b3282c63807e5b65c826d14f8c41dc
Anas Alnabahin <anasnb2002nb@gmail.com>
2023-05-21 18:51:16 +0300

    saving theme in localStorage

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import { darkTheme, lightTheme } from "./styles/theme";
 import ViewProvider from "./context/ViewContext";
 import Router from "./router";
 import { AuthContextProvider } from "./context/AuthContext";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ThemeContext from "./context/ThemeContext";
 
 function App() {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
 
   const themeConfig = {
